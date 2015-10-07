076909dfbf13a4b47a3237a02a493ab74fa63df2
Elton Viana <eltonvs@outlook.com>
2015-10-06 23:29:54 -0300

    Support for any sudoku size
    Supports any sudoku size 4x4, 9x9...

--- C/sudoku_validator.c
+++ C/sudoku_validator.c
@@ -1,4 +1,14 @@
+/*
+ * jogo_sudoku.c
+ *
+ * Author: Elton Viana
+ * E-mail: me@eltonviana.com
+ *
+ */
+
 #include <stdio.h>
+#include <math.h>
+//Biblioteca utilizada para gerar a raiz quadrada
 
 int latino(int n, int m[n][n]) {
     /*
@@ -28,7 +38,7 @@ int latino(int n, int m[n][n]) {
                  */
         }
     }
-    
+
     for (i = 0; i < n - 1; i++)
     //Percorre o vetor vet até o seu penúltimo elemento
         if (vet[i] != vet[i + 1]) return 0;
@@ -54,7 +64,7 @@ int latino(int n, int m[n][n]) {
                  */
         }
     }
-    
+
     for (i = 0; i < n - 1; i++)
     //Percorre o vetor v até o seu penúltimo elemento
         if (vet[i] != vet[i + 1]) return 0;
@@ -90,44 +100,50 @@ int soma_matriz(int n, int m[n][n]) {
     //Retorna o valor da soma dos elementos (s)
 }
 
-int valida_sudoku(int sud[9][9]) {
+int valida_sudoku(int n, int r, int sud[n][n]) {
     /*
      * Variáveis Utilizadas
      * i, j, x, y : Inteiro
      *  - Iteradores para percorrer os elementos da matriz Sudoku.
+     * soma : Inteiro
+     *  - Recebe a soma de 1 a n, que cada linha/coluna/mini-matriz deve ter
      * mini : Array de Inteiro
      *  - Armazena os valores das 9 matrizes menores da matriz Sudoku.
      */
 
-    int i, j, x, y, mini[3][3];
+    int i, j, x, y, mini[r][r], soma = 0;
 
-    if (!latino(9, sud)) return 0;
+    for (i = 1; i <= n; i++)
+        soma += i;
+        //Cria a soma de 1 a n
+
+    if (!latino(n, sud)) return 0;
     /* Verifica se a matriz Sudoku é latina.
      * Se não for, para a execução do programa
      * e retorna 0 (Falso).
      */
 
-    for (x = 0; x <= 6; x += 3)
-        for (y = 0; y <= 6; y += 3) {
+    for (x = 0; x < n; x += r)
+        for (y = 0; y < n; y += r) {
         /* Contadores extras para "deslocar" a posicao da matriz (x, y).
-         * São incrementados em 3 a cada laço para "livrar" os elementos
-         * já lidos na iteração anterior.
+         * São incrementados em raiz de n a cada laço para "livrar"
+         * os elementos já lidos na iteração anterior.
          */
-            for (i = 0; i < 3; i++)
-                for (j = 0; j < 3; j++)
-                //Percorre cada uma das 9 matrizes menores (3x3)
+            for (i = 0; i < r; i++)
+                for (j = 0; j < r; j++)
+                //Percorre cada uma das n matrizes menores
                     mini[i][j] = sud[i+x][j+y];
                     //Atribui o valor do elemento atual à matriz menor
-            if (soma_matriz(3, mini) != 45) return 0;
-            /* Verifica se a soma de cada elemento da matriz menor (3x3)
-             * do Sudoku resulta em 45 (1 + 2 + 3 + ... + 7 + 8 + 9 = 45).
-             * Se não for, para a execução do programa e retorna 0 (Falso).
+            if (soma_matriz(r, mini) != soma) return 0;
+            /* Verifica se a soma de cada elemento da matriz menor
+             * do Sudoku resulta no somatorio de 1 a n.
+             * Se não, para a execução do programa e retorna 0 (Falso).
              */
         }
 
     return 1;
-    /* Se a matriz Sudoku é latina e todas as suas matrizes menores
-     * forem latinas e com soma igual a 45, retorna 1 (Verdadeiro).
+    /* Se a matriz Sudoku é latina e todas as suas matrizes menores forem
+     * latinas e com soma igual a soma gerada, retorna 1 (Verdadeiro).
      */
 }
 
@@ -136,23 +152,43 @@ int main() {
      * Variáveis Utilizadas
      * i, j : Inteiro
      *  - Iteradores para receber os elementos da matriz Sudoku.
+     * n : Inteiro
+     *  - Quadrado Perfeito.
+     *  - Recebe o tamanho do lado do Sudoku.
+     * raiz : Inteiro
+     *  - Recebe a raiz quadrada de n.
      * sud : Array de Inteiro
      *  - Armazena os valores do sudoku.
      */
 
-    int i, j, sud[9][9];
+    int i, j, n, raiz;
+
+    //Lê o tamanho do Sudoku e gera a sua raiz
+    printf("Digite o tamanho do sudoku: ");
+    scanf("%i", &n);
+    raiz = sqrt(n);
+
+    //Repete o procedimento até que n seja um quadrado perfeito
+    while(raiz*raiz != n) {
+        printf("O lado do sudoku deve ser um quadrado perfeito: ");
+        scanf("%i", &n);
+        raiz = sqrt(n);
+    }
+
+    //Cria a matriz Sudoku
+    int sud[n][n];
 
     printf("Digite os numeros do seu Sudoku:\n");
 
     //Lê os elementos da matriz Sudoku
-    for (i = 0; i < 9; i++)
-        for (j = 0; j < 9; j++)
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
             scanf("%i", &sud[i][j]);
 
     /* Mostra "E" caso valida_sudoku seja verdadeiro (1)
      * Mostra "Nao e" caso seja falso (0)
      */
-    printf("%s um sudoku valido", valida_sudoku(sud) ? "E" : "Nao e");
+    printf("%s um sudoku valido", valida_sudoku(n, raiz, sud) ? "E" : "Nao e");
 
     return 0;
 }
