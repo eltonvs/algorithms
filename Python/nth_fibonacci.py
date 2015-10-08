2aeac9130a7c9b3e7afa66e830f976f94c00ae86
Elton Viana <eltonvs@outlook.com>
2015-10-08 15:22:28 -0300

    Initial Commit

---
+++ Python/nth_fibonacci.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+def nth_fibonacci(n):
+    return round((((1 + pow(5, 1/2))/2)**n - ((1 - pow(5, 1/2))/2)**n)/(5**(1/2)))
+
+n = int(input("Enter the nth number of the fibonacci sequence you want: "))
+print("The nth number of fibonacci sequence is:", nth_fibonacci(n))
