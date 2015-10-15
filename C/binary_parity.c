60119cdcf3d65ae351724387dde256ee5bfeb5e9
Elton Viana <eltonvs@outlook.com>
2015-10-14 21:54:38 -0300

    Added comments on function

--- C/binary_parity.c
+++ C/binary_parity.c
@@ -2,6 +2,11 @@
 
 int is_even(int n) {
     return n&1 ? 0 : 1;
+    /* Return 0 if the conjunction of n and 1 is 0. Else,
+     * the result will be 1 (or -1), and will return true.
+     * If the result isn't 0, means the last digit of n is 1,
+     * so n is odd.
+    */
 }
 
 int main() {
