2148e8f7df17eeffab80e71501d99e61e958be97
Elton Viana <eltonvs@outlook.com>
2016-01-19 22:36:10 -0300

    Used ternary <3 operators

--- Python/prime_factors.py
+++ Python/prime_factors.py
@@ -2,11 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 def prime_factors(n, f = 2):
-    if n < f:
-        return []
-    if n%f == 0:
-        return [f] + prime_factors(n/f)
-    return prime_factors(n, f+1)
+    return [] if n < f else [f] + prime_factors(n/f) if n%f == 0 else prime_factors(n, f+1)
 
 n = int(input("Enter a number to be factorized: "))
 print("The prime factors are:", prime_factors(n))
