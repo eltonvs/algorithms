3f09d105537a6ef19ca3f19a805ba9f5e50bbca8
Elton Viana <eltonvs@outlook.com>
2016-02-23 21:10:52 -0300

    Added comments to code

--- C++/tower_of_hanoi.cpp
+++ C++/tower_of_hanoi.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 void move(int n, char from, char to, char other, int *num_movements) {
 	if (n == 1) {
+		// Base Case, just 1 disc, move to destiny
 		cout << "Move from " << from << " to " << to << endl;
 	}else {
+		// Move all discs (except the base disc) to the auxiliar column (swap)
 		move(n - 1, from, other, to, num_movements);
+		// Move the base disc to the destination column
 		cout << "Move from " << from << " to " << to << endl;
+		// Move the little tower (without base) to destination column (above the base)
 		move(n - 1, other, to, from, num_movements);
 	}
+	// Add 1 movement to total (needs to result in 2^n - 1)
 	(*num_movements)++;
 }
 
