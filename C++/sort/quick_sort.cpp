5d9cc70608c4f6a3ed234a05ae993ba6d179a49b
Elton Viana <eltonvs@outlook.com>
2016-03-23 21:38:57 -0300

    Fix base case of quick_sort()

--- C++/sort/quick_sort.cpp
+++ C++/sort/quick_sort.cpp
@@ -25,7 +25,7 @@ int divide(T *V, int l, int r, bool (*comp)(T, T) = compare) {
 template <typename T>
 void quick_sort(T *V, int l, int r, bool (*comp)(T, T) = compare) {
     // Verify base case (a vector with 1 element is already sorted)
-    if (r > l+1) {
+    if (r > l) {
         // Call function to partition vector and get the pivot element
         int k = divide(V, l, r, comp);
         // Divide & Conquer: call quicksort again to sort the extremities
