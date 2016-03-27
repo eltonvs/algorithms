d7addf1e695720c2af72c8b883ee42ffb60fb186
Elton Viana <eltonvs@outlook.com>
2016-03-27 17:02:47 -0300

    Removed unnecessary code

--- C++/search/binary_search.cpp
+++ C++/search/binary_search.cpp
@@ -1,36 +1,28 @@
-#include <iostream>
-
-using namespace std;
-
 int bin_search_r(int x, int l, int r, int *v) {
-	// Gets the middle position
-	int m = (r + l)/2;
+    // Gets the middle position
+    int m = (r + l)/2;
 
-	/*
-	 * If the left edge exceed the right, stop the program (the element wasn't found)
-	 * else, if the actual element is bigger than x (to be found), search again, but only on the first-half from vector
-	 * else, if the actual element is lower than x (to be found), search again, but only on the last-half from vector
-	 * else, the actual element is equals to x, so return it
-	 */
-	return (l > r) ? -1 : *(v+m) > x ? bin_search_r(x, l, m-1, v) : *(v+m) < x ? bin_search_r(x, m+1, r, v) : m;
+    /*
+     * If the left edge exceed the right, stop the program (the element wasn't found)
+     * else, if the actual element is bigger than x (to be found), search again, but only on the first-half from vector
+     * else, if the actual element is lower than x (to be found), search again, but only on the last-half from vector
+     * else, the actual element is equals to x, so return it
+     */
+    return (l > r) ? -1 :
+        *(v+m) > x ? bin_search_r(x, l, m-1, v) :
+            *(v+m) < x ? bin_search_r(x, m+1, r, v) : m;
 }
 
 int bin_search_i(int x, int *v, int l, int r) {
-	while (l <= r) {
-		int m = (r + l)/2;
-		if (*(v+m) == x)
-			return m;
-		else if (*(v+m) < x)
-			l = m+1;
-		else
-			r = m-1;
-	}
+    while (l <= r) {
+        int m = (r + l)/2;
+        if (*(v+m) == x)
+            return m;
+        else if (*(v+m) < x)
+            l = m+1;
+        else
+            r = m-1;
+    }
 
-	return -1;
-}
-
-int main(int argc, char const *argv[]) {
-	// Your code here
-
-	return EXIT_SUCCESS;
+    return -1;
 }
--- C++/search/ternary_search.cpp
+++ C++/search/ternary_search.cpp
@@ -1,33 +1,28 @@
-#include <iostream>
-
-using namespace std;
-
 int ter_search_r(int x, int *v, int l, int r) {
     int m1 = (r+l+l)/3, m2 = (r+r+l)/3;
 
-    return (l > r) ? -1 : x == *(v+m1) ? m1 : x == *(v+m2) ? m2 : x > *(v+m2) ? ter_search_r(x, v, m2+1, r) : x < *(v+m1) ? ter_search_r(x, v, l, m1-1) : ter_search_r(x, v, m1+1, m2-1);
+    return (l > r) ? -1 :
+        x == *(v+m1) ? m1 :
+            x == *(v+m2) ? m2 :
+                x > *(v+m2) ? ter_search_r(x, v, m2+1, r) :
+                    x < *(v+m1) ? ter_search_r(x, v, l, m1-1) :
+                        ter_search_r(x, v, m1+1, m2-1);
 }
 
 int ter_search_i(int x, int *v, int l, int r) {
-	while (l <= r) {
-	    int m1 = (r+l+l)/3, m2 = (r+r+l)/3;
-		if (*(v+m1) == x)
-			return m1;
-		else if (*(v+m2) == x)
-			return m2;
-		else if (x > *(v+m2))
-			l = m2+1;
-	    else if (x < *(v+m1))
-	    	r = m1-1;
-	    else
-	    	l = m1+1, r = m2-1;
-	}
+    while (l <= r) {
+        int m1 = (r+l+l)/3, m2 = (r+r+l)/3;
+        if (*(v+m1) == x)
+            return m1;
+        else if (*(v+m2) == x)
+            return m2;
+        else if (x > *(v+m2))
+            l = m2+1;
+        else if (x < *(v+m1))
+            r = m1-1;
+        else
+            l = m1+1, r = m2-1;
+    }
 
-	return -1;
-}
-
-int main(int argc, char const *argv[]) {
-	// Your code here
-
-	return EXIT_SUCCESS;
+    return -1;
 }
